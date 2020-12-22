b43c54a0abaa92d856fcd10c28daffd6ea693164
Gabester0 <gceipper@gmail.com>
2020-12-21 19:41:42 -0500

    configuring redis-to-go to run on heroku

--- redis/redis.js
+++ redis/redis.js
@@ -1,7 +1,17 @@
 const redis = require('redis');
 const session = require('express-session');
 let RedisStore = require('connect-redis')(session);
-let redisClient = redis.createClient(process.env.REDIS_URL);
+let redisClient
+if (process.env.REDISTOGO_URL) {
+    // inside if statement
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+
+    redis.auth(rtg.auth.split(":")[1]);
+  } else {
+    redisClient = require("redis").createClient();
+  }
+// let redisClient = redis.createClient(process.env.REDIS_URL);
 const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
