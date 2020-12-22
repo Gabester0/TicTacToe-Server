5ab14a69f1ebdf4122932f93f5c4a79a5704f5bf
Gabester0 <gceipper@gmail.com>
2020-12-22 13:15:38 -0500

    cleaning up redis.js and adding explanatory comment

--- redis/redis.js
+++ redis/redis.js
@@ -1,16 +1,8 @@
 const redis = require('redis');
 const session = require('express-session');
 let RedisStore = require('connect-redis')(session);
-// let redisClient
-// if (process.env.REDISTOGO_URL) {
-//     // inside if statement
-//     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-//     redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-
-//     redis.auth(rtg.auth.split(":")[1]);
-//   } else {
-//     redisClient = require("redis").createClient();
-//   }
+// To use Redis on Heroku add the Heroku Redis add-on and point the redis client to an automatically created
+// environment variable called `process.env.REDIS_URL`
 let redisClient = redis.createClient(process.env.REDIS_URL);
 const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
 bluebird.promisifyAll(redis.RedisClient.prototype);
