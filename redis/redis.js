c2b4cfa53d54ad40b793279f765eb99b518b5004
gabester0 <gceipper@gmail.com>
2022-11-27 19:10:46 -0500

    switching redis.js to use upstash redis

--- redis/redis.js
+++ redis/redis.js
@@ -1,14 +1,23 @@
-const redis = require('redis');
-const session = require('express-session');
-let RedisStore = require('connect-redis')(session);
-// To use Redis on Heroku add the Heroku Redis add-on and point the redis client to an automatically created
-// environment variable called `process.env.REDIS_URL`
+const Redis = require('@upstash/redis');
 
-const redisUrl = process.env.REDIS_URL || 6379;
-let redisClient = redis.createClient(redisUrl);
-// let redisClient = redis.createClient();
+const redis = new Redis({
+    port: 6379,
+    family: 6,
+    host: 'tictactoe-server.internal',
+    password: 'catsRCool'
+})
 
-const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
-bluebird.promisifyAll(redis.RedisClient.prototype);
+// const session = require('express-session');
 
-module.exports = { redisClient, RedisStore };
+// let RedisStore = require('connect-redis')(session);
+// // To use Redis on Heroku add the Heroku Redis add-on and point the redis client to an automatically created
+// // environment variable called `process.env.REDIS_URL`
+
+// const redisUrl = process.env.REDIS_URL || 6379;
+// let redisClient = redis.createClient(redisUrl);
+// // let redisClient = redis.createClient();
+
+// const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
+// bluebird.promisifyAll(redis.RedisClient.prototype);
+
+module.exports = { redis };
