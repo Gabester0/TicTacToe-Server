ec285389dd9d46816313e9ff921f2ee400e6914c
gabester0 <gceipper@gmail.com>
2022-12-05 22:20:58 -0500

    not sure where I'm going with this

--- redis/redis.js
+++ redis/redis.js
@@ -4,9 +4,18 @@ let RedisStore = require('connect-redis')(session);
 // To use Redis on Heroku add the Heroku Redis add-on and point the redis client to an automatically created
 // environment variable called `process.env.REDIS_URL`
 
-const redisUrl = process.env.REDIS_URL || 6379;
-let redisClient = redis.createClient(redisUrl);
+// const redisUrl = process.env.REDIS_URL || 6379;
+let redisClient = redis.createClient({
+    url : 'us1-aware-sunbeam-37058.upstash.io',
+    port : '37058',
+    password: 'fc81697a3a79424882bded43c1488522',
+    tls: {}
+});
 // let redisClient = redis.createClient();
+
+redisClient.on("error", (err) => {
+    console.log({err})
+})
 
 const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
 bluebird.promisifyAll(redis.RedisClient.prototype);
