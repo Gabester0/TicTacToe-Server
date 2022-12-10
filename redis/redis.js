b46e39b65cc7f49d123f080f1d8d31990b8fa819
gabester0 <gceipper@gmail.com>
2022-12-10 16:22:53 -0500

    removing password

--- redis/redis.js
+++ redis/redis.js
@@ -2,13 +2,13 @@ const Redis = require('ioredis');
 const session = require('express-session');
 let RedisStore = require('connect-redis')(session);
 
-let redisClient = new Redis("rediss://default:fc81697a3a79424882bded43c1488522@us1-aware-sunbeam-37058.upstash.io:37058");
+let redisClient = new Redis(`rediss://default:${process.env.REDIS_TOKEN}@us1-aware-sunbeam-37058.upstash.io:37058`);
 
 redisClient.on("error", (err) => {
     console.log({err})
 });
 
-redisClient.set("ticTacToe", "is connecting to redis")
+redisClient.set("ticTacToe", "is connecting to redis without exposing password")
 
 // TODO: Verify this is redundant and remove
 // const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
